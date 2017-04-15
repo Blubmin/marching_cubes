3e177f7a35de596d9e80f89e0ce2bcf0ef7ef4df
Blubmin <imeeder@cox.net>
2017-04-15 05:47:33 +0100

    GUI because I could

--- marching_cubes/main.cpp
+++ marching_cubes/main.cpp
@@ -1,3 +1,6 @@
+#define _USE_MATH_DEFINES
+
+#include <cmath>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -8,6 +11,9 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include "../imgui/imgui.h"
+#include "../imgui/examples/opengl3_example/imgui_impl_glfw_gl3.h"
+
 #include "GLSL.h"
 #include "LookupTables.h"
 #include "MatrixStack.h"
@@ -17,7 +23,8 @@
 #define HALF_GRID GRID_SIZE / 2
 #define VOX_VERTS 8
 #define EDGE_VERTS 12
-
+#define MAX_ISO 5
+#define MIN_ISO -.5
 using namespace std;
 
 GLFWwindow* window;
@@ -37,7 +44,8 @@ MatrixStack M;
 MatrixStack V;
 MatrixStack P;
 
-double isovalue;
+float isovalue;
+float time_elapsed;
 
 struct pt_data {
     double value;
@@ -60,13 +68,18 @@ struct Voxel {
     GLuint edges[EDGE_VERTS];
 };
 
-Voxel voxels[GRID_SIZE - 1][GRID_SIZE - 1][GRID_SIZE- 1];
+Voxel v1[GRID_SIZE - 1][GRID_SIZE - 1];
+Voxel v2[GRID_SIZE - 1][GRID_SIZE - 1];
+
+Voxel (* current_vox)[GRID_SIZE - 1][GRID_SIZE -1 ];
+Voxel (* prev_vox)[GRID_SIZE - 1][GRID_SIZE - 1];
+
 pt_data values[GRID_SIZE][GRID_SIZE][GRID_SIZE];
 
 void render() {
     glUseProgram(prog.prog);
 
-    M.rotate(.001, vec3(0, 1, 0));
+    M.rotate(time_elapsed * M_PI / 4, vec3(0, 1, 0));
 
     glUniformMatrix4fv(prog.getUniformHandle("M"), 1, GL_FALSE, glm::value_ptr(M.topMatrix()));
     glUniformMatrix4fv(prog.getUniformHandle("V"), 1, GL_FALSE, glm::value_ptr(V.topMatrix()));
@@ -141,16 +154,6 @@ vec3 * interpolate(pt3 p1, pt3 p2, vec3* ret) {
     vec3 norm2 = normal_at(p2);
     double mu;
 
-    /*if (abs(val1) < DBL_EPSILON || abs(val1 - val2) < DBL_EPSILON) {
-        ret[0] = vec3(p1.x, p1.y, p1.z);
-        ret[1] = norm1;
-        return ret;
-    }
-    if (abs(val2) < DBL_EPSILON) {
-        ret[0] = vec3(p2.x, p2.y, p2.z);
-        ret[1] = norm2;
-        return ret;
-    }*/
     mu = (isovalue - val1) / (val2 - val1);
     ret[0].x = p1.x + mu * (p2.x - p1.x);
     ret[0].y = p1.y + mu * (p2.y - p1.y);
@@ -159,82 +162,115 @@ vec3 * interpolate(pt3 p1, pt3 p2, vec3* ret) {
     return ret;
 }
 
+int get_vert_idx(int x, int y, int z, int e) {
+    int elem = -1;
+    if (y > -HALF_GRID && e >= 0 && e <= 3) {
+        elem = (*current_vox)[x + HALF_GRID][y - 1 + HALF_GRID].edges[e + 4];
+    }
+    else if (x > -HALF_GRID && (e == 0 || e == 4 || e == 8 || e == 9)) {
+        switch (e) {
+        case 0:
+            e = 2;
+        case 4:
+            e = 6;
+        case 8:
+            e = 11;
+        case 9:
+            e = 10;
+        }
+        elem = (*current_vox)[x - 1 + HALF_GRID][y + HALF_GRID].edges[e];
+    }
+    else if (z > -HALF_GRID && (e == 3 || e == 7 || e == 8 || e == 11)) {
+        switch (e) {
+        case 3:
+            e = 1;
+        case 7:
+            e = 5;
+        case 8:
+            e = 9;
+        case 11:
+            e = 10;
+        }
+        GLuint elem = (*prev_vox)[x + HALF_GRID][y + HALF_GRID].edges[e];
+    }
+    return elem;
+}
+
+void swap_slices() {
+    Voxel (* c)[GRID_SIZE - 1][GRID_SIZE - 1] = current_vox;
+    current_vox = prev_vox;
+    prev_vox = c;
+}
+
 void compute_tris() {
     for (int x = -HALF_GRID; x < HALF_GRID - 1; x++) {
         for (int y = -HALF_GRID; y < HALF_GRID - 1; y++) {
             for (int z = -HALF_GRID; z < HALF_GRID - 1; z++) {
                 int idx = 0;
                 Voxel vox = get_voxel(x, y, z);
-                idx |= value_at(vox.verts[0]) < isovalue ? 1 << 0 : 0;
-                idx |= value_at(vox.verts[1]) < isovalue ? 1 << 1 : 0;
-                idx |= value_at(vox.verts[2]) < isovalue ? 1 << 2 : 0;
-                idx |= value_at(vox.verts[3]) < isovalue ? 1 << 3 : 0;
-                idx |= value_at(vox.verts[4]) < isovalue ? 1 << 4 : 0;
-                idx |= value_at(vox.verts[5]) < isovalue ? 1 << 5 : 0;
-                idx |= value_at(vox.verts[6]) < isovalue ? 1 << 6 : 0;
-                idx |= value_at(vox.verts[7]) < isovalue ? 1 << 7 : 0;
+
+                for (int i = 0; i < VOX_VERTS; i++) {
+                    idx |= value_at(vox.verts[i]) < isovalue ? 1 << i : 0;
+                }
 
                 vec3 vertList[EDGE_VERTS][2];
 
                 if (!edgeTable[idx]) {
+                    Voxel (*temp)[GRID_SIZE - 1] = (*current_vox);
+                    temp[x + HALF_GRID][y + HALF_GRID] = vox;
                     continue;
                 }
 
-                if (edgeTable[idx] & 1 << 0) {
-                    interpolate(vox.verts[0], vox.verts[1], vertList[0]);
-                }
-                if (edgeTable[idx] & 1 << 1) {
-                    interpolate(vox.verts[1], vox.verts[2], vertList[1]);
-                }
-                if (edgeTable[idx] & 1 << 2) {
-                    interpolate(vox.verts[2], vox.verts[3], vertList[2]);
-                }
-                if (edgeTable[idx] & 1 << 3) {
-                    interpolate(vox.verts[3], vox.verts[0], vertList[3]);
-                }
-                if (edgeTable[idx] & 1 << 4) {
-                    interpolate(vox.verts[4], vox.verts[5], vertList[4]);
-                }
-                if (edgeTable[idx] & 1 << 5) {
-                    interpolate(vox.verts[5], vox.verts[6], vertList[5]);
-                }
-                if (edgeTable[idx] & 1 << 6) {
-                    interpolate(vox.verts[6], vox.verts[7], vertList[6]);
-                }
-                if (edgeTable[idx] & 1 << 7) {
-                    interpolate(vox.verts[7], vox.verts[4], vertList[7]);
-                }
-                if (edgeTable[idx] & 1 << 8) {
-                    interpolate(vox.verts[0], vox.verts[4], vertList[8]);
-                }
-                if (edgeTable[idx] & 1 << 9) {
-                    interpolate(vox.verts[1], vox.verts[5], vertList[9]);
-                }
-                if (edgeTable[idx] & 1 << 10) {
-                    interpolate(vox.verts[2], vox.verts[6], vertList[10]);
-                }
-                if (edgeTable[idx] & 1 << 11) {
-                    interpolate(vox.verts[3], vox.verts[7], vertList[11]);
-                }
-
                 for (int i = 0; triTable[idx][i] != -1; i += 3) {
-                    
-                    vec3 vert1 = vertList[triTable[idx][i]][0];
-                    elements.push_back(verts.size());
-                    verts.push_back(vert1);
-                    norms.push_back(vertList[triTable[idx][i]][1]);
+                    int p1, p2, p3;
+                    p1 = triTable[idx][i];
+                    p2 = triTable[idx][i+1];
+                    p3 = triTable[idx][i+2];
 
-                    vec3 vert2 = vertList[triTable[idx][i + 1]][0];
-                    elements.push_back(verts.size());
-                    verts.push_back(vert2);
-                    norms.push_back(vertList[triTable[idx][i + 1]][1]);
+                    int e1, e2, e3;
+                    e1 = -1; //  get_vert_idx(x, y, z, p1);
+                    e2 = -1; //  get_vert_idx(x, y, z, p2);
+                    e3 = -1; //  get_vert_idx(x, y, z, p3);
 
-                    vec3 vert3 = vertList[triTable[idx][i + 2]][0];
-                    elements.push_back(verts.size());
-                    verts.push_back(vert3);
-                    norms.push_back(vertList[triTable[idx][i + 2]][1]);
+                    if (e1 < 0) {
+                        int i1 = interp_table[p1][0];
+                        int i2 = interp_table[p1][1];
+                        vec3 vert[2];
+                        interpolate(vox.verts[i1], vox.verts[i2], vert);
+                        e1 = verts.size();
+                        verts.push_back(vert[0]);
+                        norms.push_back(vert[1]);
+                    }
+
+                    if (e2 < 0) {
+                        int i1 = interp_table[p2][0];
+                        int i2 = interp_table[p2][1];
+                        vec3 vert[2];
+                        interpolate(vox.verts[i1], vox.verts[i2], vert);
+                        e2 = verts.size();
+                        verts.push_back(vert[0]);
+                        norms.push_back(vert[1]);
+                    }
+
+                    if (e3 < 0) {
+                        int i1 = interp_table[p3][0];
+                        int i2 = interp_table[p3][1];
+                        vec3 vert[2];
+                        interpolate(vox.verts[i1], vox.verts[i2], vert);
+                        e3 = verts.size();
+                        verts.push_back(vert[0]);
+                        norms.push_back(vert[1]);
+                    }
+                    elements.push_back(e1);
+                    elements.push_back(e2);
+                    elements.push_back(e3);
+                    vox.edges[p1] = e1;
+                    vox.edges[p2] = e2;
+                    vox.edges[p3] = e3;
                 }
+                (*current_vox)[x + HALF_GRID][y + HALF_GRID] = vox;
             }
+            swap_slices();
         }
     }
 }
@@ -249,10 +285,33 @@ void march() {
 
 }
 
+void refresh() {
+    march();
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_vert);
+    glBufferData(GL_ARRAY_BUFFER, verts.size() * sizeof(glm::vec3), &verts[0], GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_norm);
+    glBufferData(GL_ARRAY_BUFFER, norms.size() * sizeof(glm::vec3), &norms[0], GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, elements.size() * sizeof(GLuint), &elements[0], GL_STATIC_DRAW);
+
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
+
 void init() {
     int width, height;
     glfwGetFramebufferSize(window, &width, &height);
     float aspect = width / (float)height;
+
+    v1[0][0] = Voxel();
+    current_vox = &v1;
+    prev_vox = &v2;
 
     prog = Program("./vert.glsl", "./frag.glsl");
     march();
@@ -312,14 +371,12 @@ void run() {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    window = glfwCreateWindow(640, 480, "Marching Cubes", NULL, NULL);
+    window = glfwCreateWindow(1280, 720, "Marching Cubes", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         throw runtime_error("glfwCreateWindow failed");
     }
-
-    glfwSetKeyCallback(window, key_callback);
 
     glfwMakeContextCurrent(window);
 
@@ -330,6 +387,8 @@ void run() {
 
     // GLEW throws some errors, so discard all the errors so far
     while (glGetError() != GL_NO_ERROR) {}
+
+    ImGui_ImplGlfwGL3_Init(window, true);
 
     glEnable(GL_DEPTH_TEST);
     //glfwSwapInterval(1);
@@ -343,16 +402,27 @@ void run() {
 
     while (!glfwWindowShouldClose(window))
     {
+        glfwPollEvents();
+        ImGui_ImplGlfwGL3_NewFrame();
+
+        ImGui::Begin("Set Iso Level");
+        ImGui::SliderFloat("Iso Level", &isovalue, MIN_ISO, MAX_ISO);
+        if (ImGui::Button("March")) {
+            refresh();
+        }
+        ImGui::End();
+
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         double newTime = glfwGetTime();
-        double timeElapsed = newTime - oldTime;
+        time_elapsed = newTime - oldTime;
 
         oldTime = newTime;
         render();
 
+        ImGui::Render();
         glfwSwapBuffers(window);
-        glfwPollEvents();
+        
     }
     glfwDestroyWindow(window);
     glfwTerminate();
